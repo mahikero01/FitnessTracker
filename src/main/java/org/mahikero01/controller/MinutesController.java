d0fcd08829286e3d3a5f41249f46b60693d59098
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-12-19 13:45:50 +0800

    4 - Controller
    Build Controller

---
+++ src/main/java/org/mahikero01/controller/MinutesController.java
@@ -0,0 +1,16 @@
+package org.mahikero01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MinutesController {
+	
+	@RequestMapping(value = "/addMinutes")
+	public String addMinutes() {
+		
+		return "addMinutes";
+		
+	}
+	
+}
