58420a45d3167ac489ff18c0053082c3a8ea7201
Sarmiento <sarmife@prd.manulifeusa.com>
2017-12-19 11:12:46 -0500

    5 - Views
    Chaining

--- src/main/java/org/mahikero01/controller/MinutesController.java
+++ src/main/java/org/mahikero01/controller/MinutesController.java
@@ -7,13 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MinutesController {
-	
+
 	@RequestMapping(value = "/addMinutes")
-	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
-		
+	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
+
 		System.out.println("exercise: " + exercise.getMinutes());
-		return "addMinutes";
-		
+		//return "forward:addMoreMinutes.html";
+		return "redirect:addMoreMinutes.html";
+
 	}
-	
+
+	@RequestMapping(value = "/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+
+		System.out.println("exercising: " + exercise.getMinutes());
+		return "addMinutes";
+
+	}
+
 }
