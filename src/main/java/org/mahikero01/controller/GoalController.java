4f085db2be9586a9b599e388b01d00c4d6fd0fc3
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-12-20 03:43:20 +0800

    up

--- src/main/java/org/mahikero01/controller/GoalController.java
+++ src/main/java/org/mahikero01/controller/GoalController.java
@@ -5,7 +5,7 @@ import javax.validation.Valid;
 import org.mahikero01.model.Goal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+//import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,8 +25,8 @@ public class GoalController {
 	}
 
 	@RequestMapping(value = "addGoal", method = RequestMethod.POST)
-	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
-		System.out.println("result has errors: " + result.hasErrors());
+	public String updateGoal( @ModelAttribute("goal") Goal goal) {
+		//System.out.println("result has errors: " + result.hasErrors());
 		
 		System.out.println("Minutes updated: " +goal.getMinutes());
 		
